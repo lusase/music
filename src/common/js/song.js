0e47bd78e6ab72b86100f283286925d15f4ccb4e
qtxz2000 <wenkaizhijia@163.com>
2018-03-12 22:40:36 +0800

    fix: bug nodef

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -53,6 +53,6 @@ export function createSong(data) {
     duration: data.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${data.albummid}.jpg?max_age=2592000`,
     // url: `http://ws.stream.qqmusic.qq.com/${data.songid}.m4a?fromtag=46`,
-    url: `http://isure.stream.qqmusic.qq.com/C100${musicData.songmid}.m4a?fromtag=32`
+    url: `http://isure.stream.qqmusic.qq.com/C100${data.songmid}.m4a?fromtag=32`
   })
 }
